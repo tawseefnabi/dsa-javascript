9e01025e604811f66ad44c59523810688eb9fbb7
“tawseef <tawseefnabi9@gmail.com>
2023-08-26 23:59:00 +0530

    finding duolcates in array

---
+++ duolicateInArray.js
@@ -0,0 +1,37 @@
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+// There is only one repeated number in nums, return this repeated number.
+
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+// Example 2:
+
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+
+duplicateInArray = (arr) => {
+  for(let i =0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+      if(arr[i]==arr[j]){
+        console.log(`for array ${arr} duplicate is `, arr[i]);
+      }
+    }
+  }
+}
+duplicateInArray_v2 = (arr) =>{
+  arr.sort((a,b) => a-b)
+  arr.forEach((element, i) => {
+      if(element ==  arr[i+1]){
+        console.log(`for array ${arr} duplicate is `, element);
+      }
+  });
+
+}
+duplicateInArray( [1,3,4,2,2])
+duplicateInArray_v2( [3,1,3,4,2])
