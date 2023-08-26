e4360661ac61a90c97214cb824bf7310632e3dc4
“tawseef <tawseefnabi9@gmail.com>
2023-08-26 21:47:37 +0530

    occrence in array

--- occurenceInArray.js
+++ occurenceInArray.js
@@ -14,8 +14,23 @@
 //   Input: arr[] = {1, 1, 2, 2, 2, 2, 3,},   x = 4
 //   Output: -1 // 4 doesn't occur in arr[] 
 
-occurenceInArray =(arr, n)=>{
-  console.log(arr.filter(a => a== n).length)
+// v1
+occurenceInArray = (arr, n) =>{
+  console.log("count: ", arr.filter(a => a== n).length);
 }
 occurenceInArray([1, 1, 2, 2, 2, 2, 3], 4)
 occurenceInArray([1, 1, 2, 2, 2, 2, 3],   2)
+// v2
+occurenceInArray_v2 =(arr, n)=>{  
+  let count = 0
+  arr.forEach(element => {
+    if(element == n){
+      count++
+    }
+  });
+  console.log('count: ' + count);
+}
+occurenceInArray_v2([1, 1, 2, 2, 2, 2, 3], 4)
+occurenceInArray_v2([1, 1, 2, 2, 2, 2, 3],   2)
+
+
