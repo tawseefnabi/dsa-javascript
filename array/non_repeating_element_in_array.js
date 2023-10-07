52c74fc02df6ade2c76abd31dff8812b8a2b132a
“tawseef <tawseefnabi9@gmail.com>
2023-10-07 11:14:55 +0530

    non_repeating element in array

--- array/non_repeating_element_in_array.js
+++ array/non_repeating_element_in_array.js
@@ -11,6 +11,27 @@ function non_repeating_element_in_array(arr){
 arr =[10, 5, 3, 4, 3, 5, 6]
 
 
-non_repeating_element_in_array(arr)
+// non_repeating_element_in_array(arr)
 arr1 = [-1, 2, -1, 3, 0]
-non_repeating_element_in_array(arr1)
+// non_repeating_element_in_array(arr1)
+
+
+function non_repeating_element_in_array_v2(arr){
+  console.log("arr", arr);
+  const elementCount = {}
+  arr.forEach(element => {
+    if(elementCount[element]){
+      elementCount[element]++
+    }else{
+      elementCount[element] =1
+    }
+  });
+  for(let i=0;i<arr.length;i++){
+    if(elementCount[arr[i]] ==1){
+      return console.log(arr[i])
+    }
+  }
+  return console.log("no element ");
+}
+ arr = [9, 4, 9, 6, 7, 4];
+non_repeating_element_in_array_v2(arr)
